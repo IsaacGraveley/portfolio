8d071549929254a0d15dbc1aa1678bf83ff38069
IsaacGraveley <igraveley@hotmail.com>
2020-11-26 13:17:01 -0500

    bg video added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <video className="bgvideo" loop autoplay>
+          <source src="../video.mp4" type="video/mp4" />
+        </video>
         <h1>Isaac Graveley</h1>
         <p>Portfolio Website under construction :) test cname build</p>
       </header>
