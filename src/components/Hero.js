24b6cfb65462ae8e2eed8819ab576c9c5f9fa476
IsaacGraveley <igraveley@hotmail.com>
2020-11-26 21:53:22 -0500

    under construction

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ class Hero extends React.Component {
       <header className="App-header bg-gray-50 container mx-auto">
         <div className="header-text text-center sm:text-left">
           <h1 className="text-indigo-700 font-black text-6xl">Isaac Graveley</h1>
-          <p>Portfolio Website under construction :)/p>
+          <p>Portfolio Website under construction :) </p>
         </div>
       </header>
     );
