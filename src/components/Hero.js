05ca10fad249291817b1c87c1b30c40068748fce
= <igraveley@hotmail.com>
2021-07-31 22:24:35 -0400

    updating nav

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Hero = () => {
+  return (
+    <div class="bg-white h-screen flex flex-col justify-center">
+
+      <h1 class="lg:text-9xl md:text-7xl sm:text-5xl text-center text-3xl font-black mb-14">EGGCELENT</h1>
+    <Link></Link>
+    </div>
+  )
+}
+
+export default Hero;
