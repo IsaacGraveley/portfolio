4bcb072dc433179e7c501edfcd8334569415b506
= <igraveley@hotmail.com>
2021-07-31 22:29:58 -0400

    updating hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,8 @@ const Hero = () => {
   return (
     <div class="bg-white h-screen flex flex-col justify-center">
 
-      <h1 class="lg:text-9xl md:text-7xl sm:text-5xl text-center text-3xl font-black mb-14">EGGCELENT</h1>
+      <h1 class="lg:text-9xl md:text-7xl sm:text-5xl text-center text-3xl font-black mb-14">Isaac Graveley</h1>
+      <h2 class="lg:text-5xl md:text-3xl sm:text-xl text-center text-xl font-black mb-14">Portfolio Website Under Construction</h2>
     <Link></Link>
     </div>
   )
