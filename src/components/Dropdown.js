5cce093eb5bcb0f3cfd3a72e8057e96696a3b6b8
= <igraveley@hotmail.com>
2021-07-31 22:37:33 -0400

    removed bg on drop down menu

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const Dropdown = ({isOpen, toggle}) => {
     return (
-        <div className={isOpen ? 'grid grid-rows-4 text-center items-center bg-yellow-500' : 'hidden'} onClick={toggle}>
+        <div className={isOpen ? 'grid grid-rows-4 text-center items-center' : 'hidden'} onClick={toggle}>
         
         <Link className="p-4" to="/">Home</Link>
         <Link className="p-4" to="/menu">Projects</Link>
